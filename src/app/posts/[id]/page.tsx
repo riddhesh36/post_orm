e80c3cd1ec2e39a6320c6de8d14df857ca3d532e
riddhesh36 <34064118+riddhesh36@users.noreply.github.com>
2025-10-06 15:47:11 +0530

    last change 2

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,23 +2,14 @@ import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = params;
-
-  const post = await prisma.post.findUnique({
-    where: {
-      id,
-    },
-  });
-
-  if (!post) {
-    return notFound();
-  }
-
+export default async function Page({ params }: any) {
+    const { id } = params;
+  
+    const post = await prisma.post.findUnique({
+      where: { id },
+    });
+  
+    if (!post) return notFound();
   return (
     <div className="text-center pt-12">
       <h1 className="text-bold text-5xl capitalize text-purple-600">{post.title}</h1>
